8f351d4ac24dd9a60f4772eb81e766b28f4547f8
KiloByte <kirieshka2350@gmail.com>
2024-11-13 15:35:32 +0700

    do a lab?

--- lab2_1.c
+++ lab2_1.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
 #include <mpi.h>
+#include <math.h>
 
-double func (double x, double y)
+double func (double x)
 {
-
+    return x*x*x*x / (((x*x) / 2) + x + 6);
 }
 
 int main (int argc, char* argv[])
 {
-    MPI_init (&argc, &argv);
+    MPI_Init (&argc, &argv);
     int rank, commsize;
     double eps = 1E-6;
-    double h;
-    double a;
-    double b;
-    double n = 0.01;
+    double n = 100;
+    double a = 0.4;
+    double b = 1.5;
+    double h = (b - a) / n;
+    double new_sum = 0.0;
     MPI_Comm_size(MPI_COMM_WORLD, &commsize);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    for(double i = a; i <= b;)
+    double diff = 0.0;
+    double start = MPI_Wtime();
+    for (int presc = 1; diff < eps; presc++)
     {
-        
+        double sum = 0.0;
+        int my_left = presc * ((n / commsize) * rank);
+        int my_right = (rank != commsize - 1) ? ((presc * (n / commsize) * (rank + 1)) - 1) : (n * presc - 1);
+        for (int i = my_left; i <= my_right; i++)
+        {
+            sum += func(a + (h / 2) * i);
+        }
+        MPI_Allreduce(&sum, &new_sum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        new_sum = new_sum * h;
+        diff = new_sum - diff;
     }
+    double end = MPI_Wtime() - start;
+    if (rank == 0)
+        printf("Result is: %f\n clculated in %f\n", new_sum, end);
     MPI_Finalize();
     return 0;
 }
--- lab2_2.c
+++ lab2_2.c
@@ -1,2 +1,41 @@
 #include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 #include <mpi.h>
+
+int main(int argc, char** argv)
+{
+    MPI_Init(&argc, &argv);
+    const double PI = 3.14159265358979323846;
+    int n = 1000000;
+    int all_in = 0;
+    int n_all_in;
+    double sum = 0.0;
+    double n_sum;
+    int rank, commsize;
+    MPI_Comm_size(MPI_COMM_WORLD, &commsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    srand(rank);
+    double start = MPI_Wtime();
+    for (int k = 0; k < n; k += commsize)
+    {
+        double x = (-1) * ((double)rand() / RAND_MAX);
+        double y = (double)rand() / RAND_MAX;
+        if (y <= sin(x))
+        {
+            all_in++;
+            sum = sum + exp(x - y);
+        }
+    }
+    MPI_Reduce(&sum, &n_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&all_in, &n_all_in, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0)
+    {    
+        double v = PI * n_all_in / n;
+        double res = v * n_sum / n_all_in;
+        double end = MPI_Wtime() - start;
+        printf("result is: %.12f\n n is: %d\n all dots inside: %d, time of work is: %f\n", res, n, n_all_in, end);
+    }
+    MPI_Finalize();
+    return 0;
+}
